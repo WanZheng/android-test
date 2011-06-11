1fa803474e150c2f91ff4b6ade45d2f11cc26b8a
Wilson Wan <cos.wan@gmail.com>
2011-06-11 19:00:13 +0800

    create a empty project, drag

---
+++ drag/src/drag/cos/drag.java
@@ -0,0 +1,15 @@
+package drag.cos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class drag extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
