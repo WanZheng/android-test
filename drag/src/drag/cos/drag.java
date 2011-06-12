1adf8012e2ab8ac11a951184678ffbc588698e6b
Wilson Wan <cos.wan@gmail.com>
2011-06-12 22:41:50 +0800

    try to implement a DragHost

--- drag/src/drag/cos/drag.java
+++ drag/src/drag/cos/drag.java
@@ -2,6 +2,79 @@ package drag.cos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.net.Uri;
+import android.graphics.Paint;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PaintFlagsDrawFilter;
+import android.content.Context;
+import android.util.Log;
+import android.view.animation.RotateAnimation;
+import android.view.animation.TranslateAnimation;
+import android.view.animation.Animation;
+import android.view.MotionEvent;
+
+class DragHost extends FrameLayout {
+    private View mHostedView;
+    private View mDragItem;
+
+    public DragHost(Context context) {
+	super(context);
+    }
+
+    public void setHostedView(View hostedView) throws Exception{
+	if (mHostedView != null) {
+	    throw new Exception("has a hosted view already");
+	}
+	mHostedView = hostedView;
+	addView(hostedView);
+    }
+
+    @Override protected void onLayout(boolean changed, int l, int t, int r, int b) {
+	if (mHostedView != null) {
+	    Log.d("cos", "hostedView.layout("+l+","+t+","+r+","+b+")");
+	    mHostedView.layout(0, 0, r-l, b-t);
+	    Log.d("cos", "done");
+	}else{
+	    Log.d("cos", "hostedView == null");
+	}
+    }
+
+    /*
+    @Override protected void onDraw(Canvas canvas) {
+	Log.d("cos", "DragHost.onDraw()");
+	//super.draw(canvas);
+	//mHostedView.draw(canvas);
+
+	Paint paint = new Paint();
+	paint.setColor(Color.RED);
+	canvas.drawRect(0, 0, getWidth(), getHeight(), paint);
+    }
+
+    @Override public void draw(Canvas canvas) {
+	Log.d("cos", "DragHost.draw()");
+	// super.draw(canvas);
+    }
+    */
+
+    @Override protected void dispatchDraw(Canvas canvas) {
+	Log.d("cos", "dispatchDraw()");
+	super.dispatchDraw(canvas);
+
+	Paint paint = new Paint();
+	paint.setColor(Color.GREEN);
+	canvas.drawRect(0, 0, 50, 100, paint);
+    }
+}
 
 public class drag extends Activity
 {
@@ -10,6 +83,43 @@ public class drag extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+	DragHost dragHost = new DragHost(this);
+        LinearLayout layout = new LinearLayout(this);
+	layout.setOrientation(LinearLayout.VERTICAL);
+
+        RectView view = new RectView(this, Color.BLUE);
+        layout.addView(view, 300, 200);
+
+	view = new RectView(this, Color.RED);
+	layout.addView(view, 200, 300);
+
+	try {
+	    dragHost.setHostedView(layout);
+	} catch (Exception e){
+	    Log.d("cos", "" + e);
+	}
+
+	setContentView(dragHost);
+    }
+
+    class RectView extends View {
+        private Paint mPaint;
+        private int mCount = 0;
+
+        RectView(Context context, int color) {
+            super(context);
+	    mPaint = new Paint();
+	    mPaint.setColor(color);
+	    mPaint.setAntiAlias(true);
+        }
+
+        @Override protected void onDraw(Canvas canvas) {
+            canvas.drawRect(0, 0, getWidth(), getHeight(), mPaint);
+            mCount++;
+        }
+
+        public int getCount() { return mCount; }
+        public void resetCount() { mCount = 0; }
     }
 }
