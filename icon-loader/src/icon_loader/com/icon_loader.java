dbd530b2de8fbd7963ea15131620737bf5cffbd8
Wilson Wan <wanzheng@minigui.org>
2011-07-06 14:15:35 +0800

    add project icon-loader

---
+++ icon-loader/src/icon_loader/com/icon_loader.java
@@ -0,0 +1,15 @@
+package icon_loader.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class icon_loader extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
