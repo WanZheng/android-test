034352517c48f53021c63839dde14624c4b7f284
Wilson Wan <wanzheng@minigui.org>
2011-07-18 10:38:52 +0800

    add project animation

---
+++ animation/src/animation/cos/com/Animation.java
@@ -0,0 +1,15 @@
+package animation.cos.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Animation extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
