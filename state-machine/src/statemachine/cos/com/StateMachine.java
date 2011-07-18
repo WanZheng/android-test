91a6d60a4bcd594f3956ee631ce8a138a4bb4f17
Wan Zheng <cos.register@gmail.com>
2011-07-18 15:26:59 +0800

    create project state-machine

---
+++ state-machine/src/statemachine/cos/com/StateMachine.java
@@ -0,0 +1,15 @@
+package statemachine.cos.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StateMachine extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
